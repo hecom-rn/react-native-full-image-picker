98eb8c9854c44da6f618213fa0fd23c20d2fbae2
liuzetan <liuzetan1988@126.com>
2020-07-29 09:33:02 +0800

    revert

--- src/PhotoModalPage.js
+++ src/PhotoModalPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Modal, BackHandler, InteractionManager, Platform } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 import PageKeys from './PageKeys';
 import CameraView from './CameraView';
 import AlbumListView from './AlbumListView';
@@ -43,15 +42,10 @@ export default class extends React.PureComponent {
             [PageKeys.album_view]: AlbumView,
             [PageKeys.preview]: PreviewMultiView,
         };
-        const defaultProp = {
-            ...this.props,
-            callback: callback,
-        }
         const withUnwrap = (WrappedComponent) => class extends React.PureComponent {
             render() {
                 return (
                     <WrappedComponent
-                        {...defaultProp}
                         {...this.props.route.params}
                         navigation={this.props.navigation}
                     />
@@ -65,30 +59,30 @@ export default class extends React.PureComponent {
                     <Stack.Screen
                         name={cur}
                         component={withUnwrap(allscenes[cur])}
-                        options={()=> {
-                            return { gesturesEnabled: false }
-                        }}
+                        options={{ gesturesEnabled: false }}
                     />
                 );
                 return prv;
             }, []);
 
         const NavigationDoor = (
-            <Stack.Navigator
+            <Stack.navigation
                 initialRouteName={this.props.initialRouteName}
+                initialRouteParams={{
+                    ...this.props,
+                    callback: callback,
+                }}
                 headerMode={'none'}
             >
                 {scenes}
-            </Stack.Navigator>
+            </Stack.navigation>
         )
         return (
             <Modal
                 animationType={'slide'}
                 supportedOrientations={this.props.supportedOrientations}
             >
-                <NavigationContainer>
-                    {NavigationDoor}
-                </NavigationContainer>
+                {NavigationDoor}
             </Modal>
         );
     }
