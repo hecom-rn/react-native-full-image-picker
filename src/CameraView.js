584a7f3745e4ab2f3ddefe7659269d332b8842f2
Ma Shuai <mashuai_hy@163.com>
2023-08-21 11:39:00 +0800

    fix: 处理Android部分设备水印没和取景器底部重合

--- src/CameraView.js
+++ src/CameraView.js
@@ -112,7 +112,7 @@ export default class extends React.PureComponent {
                     fixOrientation={true}
                 >
                     {this.props.waterView && (
-                        <ViewShot ref={ref => this.viewShot = ref} style={{ flex: 1 }}>
+                        <ViewShot ref={ref => this.viewShot = ref} style={{ flex: 1, backgroundColor: 'transparent', marginBottom: -2 }}>
                             {this.props.waterView?.()}
                         </ViewShot>
                     )}
