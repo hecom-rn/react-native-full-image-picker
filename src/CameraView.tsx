e15da5ddb3769678bfaa0d68af5b8ab094fe6da8
Ma Shuai <mashuai_hy@163.com>
2026-05-09 17:49:21 +0800

    fix: 修复iOS拍照 文件path前缀不应包含file://

--- src/CameraView.tsx
+++ src/CameraView.tsx
@@ -288,7 +288,7 @@ export default function CameraView(props: Props): React.ReactElement {
                     itemPath, item.height, item.width, 'JPEG', 100, 0,
                 );
                 item = { ...item, ...rotatedImage };
-                itemPath = rotatedImage.path;
+                itemPath =  Platform.OS === 'ios' ? rotatedImage.path : rotatedImage.uri;
             }
 
             // Watermark handling
